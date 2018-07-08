0918f024792f2e220c39d83b883ada8f24367fd3
Joey Yakimowich-Payne <jyapayne@gmail.com>
2018-07-08 22:50:34 +0900

    Remove dir sep from replace

--- nimgen.nim
+++ nimgen.nim
@@ -585,7 +585,7 @@ proc c2nim(fl, outfile: string, c2nimConfig: c2nimConfigObj) =
   passC &= "import ospaths\n"
 
   for inc in gIncludes:
-    let relativeInc = inc.replace(gOutput & $DirSep, "")
+    let relativeInc = inc.replace(gOutput, "")
     passC &= (
       """{.passC: "-I\"" & currentSourcePath().splitPath().head & "/$#\"".}""" %
       [relativeInc]
@@ -595,7 +595,7 @@ proc c2nim(fl, outfile: string, c2nimConfig: c2nimConfigObj) =
     outpragma &= "{." & prag & ".}\n"
 
   let fname = file.splitFile().name.replace(re"[\.\-]", "_")
-  let fincl = file.replace(gOutput & $DirSep, "")
+  let fincl = file.replace(gOutput, "")
 
   if c2nimConfig.dynlib.len() != 0:
     let
